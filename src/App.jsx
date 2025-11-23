dd78c367876dcb0241abc03157b14151a8479922
jajady <mamon0502@gmail.com>
2025-11-23 23:30:04 +0900

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { LoadScript } from "@react-google-maps/api";
 import { API_KEY } from './api/googleMapApi.js';
 import Intro from './pages/intro/Intro.jsx';
@@ -21,98 +21,95 @@ const LIBRARIES = ["places", "marker"];
 
 function App() {
   return (
-    <BrowserRouter
-      basename="/trip-blocks-deploy">
-      <LoadScript
-        id="google-map-script"
-        googleMapsApiKey={API_KEY}
-        libraries={LIBRARIES}
-      >
-        <Routes>
-          {/* 로그인 불필요 */}
-          <Route path="/" element={<Intro />} />
-          <Route path="/signin" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
+    <LoadScript
+      id="google-map-script"
+      googleMapsApiKey={API_KEY}
+      libraries={LIBRARIES}
+    >
+      <Routes>
+        {/* 로그인 불필요 */}
+        <Route path="/" element={<Intro />} />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
 
-          {/* 로그인 필요 */}
-          <Route
-            path="/signup-success"
-            element={
-              <PrivateRoute>
-                <SignUpSuccessPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/delete-account"
-            element={
-              <PrivateRoute>
-                <DeleteAccount />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/mypage"
-            element={
-              <PrivateRoute>
-                <MyPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/create-calendar"
-            element={
-              <PrivateRoute>
-                <CreateCalendar />
-              </PrivateRoute>
-            }
-          />
+        {/* 로그인 필요 */}
+        <Route
+          path="/signup-success"
+          element={
+            <PrivateRoute>
+              <SignUpSuccessPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/delete-account"
+          element={
+            <PrivateRoute>
+              <DeleteAccount />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            <PrivateRoute>
+              <MyPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/create-calendar"
+          element={
+            <PrivateRoute>
+              <CreateCalendar />
+            </PrivateRoute>
+          }
+        />
 
-          <Route
-            path="/edit-profile"
-            element={
-              <PrivateRoute>
-                <EditProfile />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/edit-cover"
-            element={
-              <PrivateRoute>
-                <EditCover />
-              </PrivateRoute>
-            }
-          />
+        <Route
+          path="/edit-profile"
+          element={
+            <PrivateRoute>
+              <EditProfile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit-cover"
+          element={
+            <PrivateRoute>
+              <EditCover />
+            </PrivateRoute>
+          }
+        />
 
-          {/* ✅ 여기 calendarId를 파라미터로 받도록 변경 */}
-          <Route
-            path="/calendar/:calendarId"
-            element={
-              <PrivateRoute>
-                <Calendar />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/calendar/:calendarId/route"
-            element={
-              <PrivateRoute>
-                <RouteMode />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/calendar/:calendarId/setting"
-            element={
-              <PrivateRoute>
-                <SettingCalendar />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </LoadScript>
-    </BrowserRouter>
+        {/* ✅ 여기 calendarId를 파라미터로 받도록 변경 */}
+        <Route
+          path="/calendar/:calendarId"
+          element={
+            <PrivateRoute>
+              <Calendar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/calendar/:calendarId/route"
+          element={
+            <PrivateRoute>
+              <RouteMode />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/calendar/:calendarId/setting"
+          element={
+            <PrivateRoute>
+              <SettingCalendar />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </LoadScript>
   );
 }
 
