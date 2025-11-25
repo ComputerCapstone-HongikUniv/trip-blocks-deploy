73e044778a449d4295458e92414379b8b327818d
jajady <mamon0502@gmail.com>
2025-11-25 19:33:34 +0900

    예시 설명

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -24,7 +24,7 @@ function Intro() {
         <div className="start-section">
           <button
             className="start-btn"
-            onClick={() => navigate("/signin")}   // ✅ 이제 정상 작동
+            onClick={() => navigate("/signin")}   // 이제 정상 작동
           >
             시작하기
           </button>
@@ -32,8 +32,30 @@ function Intro() {
 
         <div className="intro-example-images">
           <img className="calendar-mode-img" src={calendarModeImg} alt="캘린더 모드" />
+
+          <div className="text-align-center">
+            <h1>장소 검색부터 일정 생성까지, 한 번에
+            </h1>
+            <span>하나의 화면에서 가고 싶은 장소를 바로 일정으로 생성해 보세요. <br />해당 장소 정보가 반영된 일정이 생성되며, 영업 시간 및 이동 시간 관련 경고가 제공됩니다.</span>
+          </div>
+
           <img className="map-mode-img" src={mapModeImg} alt="지도 모드" />
+
+          <div className="text-align-center">
+            <h1>지도모드</h1>
+            <span>추천 장소 및 찜한 장소,
+              일정에 추가한 장소를
+              모두 확인할 수 있습니다.</span>
+          </div>
+
           <img className="detail-route-img" src={detailRouteImg} alt="상세 경로 모드" />
+
+          <div className="text-align-center">
+            <h1>상세 경로 모드
+            </h1>
+            <span>일정 간의 상세한 이동 경로를 확인하고,<br />
+              이동수단을 변경해보며 다양한 경로를 탐색할 수 있습니다.</span>
+          </div>
         </div>
 
         <div className="saying">
